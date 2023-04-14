a63f48cef04e462a301433ba46d18929bcf898d0
Nguyen Tien Tung <ntt091103@gmail.com>
2023-04-14 22:33:19 +0700

    them 3 anh

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,9 +1,18 @@
 $('.slide').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     arrows: false,
     dots: true
+});
+
+$('.about-slide').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
 });
