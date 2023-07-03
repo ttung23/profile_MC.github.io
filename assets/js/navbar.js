2553d3f54646e4cabbad3bf33613c8f5c19a6454
Nguyen Tien Tung <ntt091103@gmail.com>
2023-07-03 10:10:52 +0700

    xoa slide & jquery

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,13 +1,27 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop()) {
-            $('.navbar').addClass('sticky')
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop()) {
+//             $('.navbar').addClass('sticky')
+//         } else {
+//             $('.navbar').removeClass('sticky')
+//         }
+
+//         if (Math.ceil($(this).scrollTop() + $(this).height()) == $(document).height()) {
+//             $('.navbar').removeClass('sticky')
+//         }
+//     })
+// })
+
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 0) {
+            document.querySelector('.navbar').classList.add('sticky');
         } else {
-            $('.navbar').removeClass('sticky')
+            document.querySelector('.navbar').classList.remove('sticky');
         }
 
-        if (Math.ceil($(this).scrollTop() + $(this).height()) == $(document).height()) {
-            $('.navbar').removeClass('sticky')
+        if (Math.ceil(window.pageYOffset + window.innerHeight) === document.documentElement.scrollHeight) {
+            document.querySelector('.navbar').classList.remove('sticky');
         }
-    })
-})
+    });
+});
